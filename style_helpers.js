88a223865574e3d309647329951afd793a48fff3
root <showell30@yahoo.com>
2023-04-30 13:11:13 +0000

    progress

---
+++ style_helpers.js
@@ -0,0 +1,53 @@
+window.style_helpers = (function () {
+
+    function style_generic_table(table) {
+        setStyles(table, {
+            border: "1px solid black",
+            borderCollapse: "collapse",
+            margin: "auto",
+            display: "block",
+        });
+
+        for (const th of table.querySelectorAll("th")) {
+            setStyles(th, {
+                padding: "4px",
+                position: "sticky",
+                top: "0px",
+                border: "1px solid black",
+            });
+        }
+    }
+
+    function style_generic_td(td) {
+        setStyles(td, {
+            border: "1px solid black",
+            padding: "4px",
+        });
+        return td;
+    }
+
+    function setStyles(elem, styles) {
+        const info = [];
+        const label = elem.id
+            ? `#${elem.id}`
+            : elem.className
+            ? `.${elem.className}`
+            : elem.tagName;
+
+        info.push(`\nSetting styles for ${label}\n`);
+
+        for (const [f, v] of Object.entries(styles)) {
+            info.push(`  ${f}: ${v};`);
+            elem.style[f] = v;
+        }
+
+        // console.trace(info.join("\n"));
+        return elem;
+    }
+
+    return {
+        style_generic_table,
+        style_generic_td,
+        setStyles,
+    };
+})();
--- table.js
+++ table.js
@@ -5,6 +5,12 @@ const {
     maybe_stripe,
 } = window.dom_helpers;
 
+const {
+    style_generic_table,
+    style_generic_td,
+    setStyles,
+} = window.style_helpers;
+
 build_person_table();
 build_fruits_table();
 build_prime_table();
@@ -422,47 +428,3 @@ function build_integer_table_widget({ number_store_callback, maxHeight }) {
     };
 }
 
-function style_generic_table(table) {
-    setStyles(table, {
-        border: "1px solid black",
-        borderCollapse: "collapse",
-        margin: "auto",
-        display: "block",
-    });
-
-    for (const th of table.querySelectorAll("th")) {
-        setStyles(th, {
-            padding: "4px",
-            position: "sticky",
-            top: "0px",
-            border: "1px solid black",
-        });
-    }
-}
-
-function style_generic_td(td) {
-    setStyles(td, {
-        border: "1px solid black",
-        padding: "4px",
-    });
-    return td;
-}
-
-function setStyles(elem, styles) {
-    const info = [];
-    const label = elem.id
-        ? `#${elem.id}`
-        : elem.className
-        ? `.${elem.className}`
-        : elem.tagName;
-
-    info.push(`\nSetting styles for ${label}\n`);
-
-    for (const [f, v] of Object.entries(styles)) {
-        info.push(`  ${f}: ${v};`);
-        elem.style[f] = v;
-    }
-
-    // console.trace(info.join("\n"));
-    return elem;
-}
