47eb32dd76a40c369854c3034c8302e7a158aef6
root <showell30@yahoo.com>
2023-04-30 13:04:33 +0000

    progress

---
+++ dom_helpers.js
@@ -0,0 +1,43 @@
+window.dom_helpers = (function () {
+
+    function dom_empty_table() {
+        const table = document.createElement("table");
+        const thead = document.createElement("thead");
+        table.append(thead);
+
+        const tbody = document.createElement("tbody");
+        table.append(tbody);
+
+        return { table, thead, tbody };
+    }
+
+    function dom_tr(...child_elems) {
+        const tr = document.createElement("tr");
+        tr.append(...child_elems);
+        return tr;
+    }
+
+    function dom_td({ id, elem }) {
+        const td = document.createElement("td");
+        td.id = id;
+        td.append(elem);
+        return td;
+    }
+
+    function maybe_stripe(elem, i, color) {
+        if (i % 2) {
+            setStyles(elem, {
+                background: color,
+            });
+        }
+
+        return elem;
+    }
+
+    return {
+        dom_empty_table,
+        dom_tr,
+        dom_td,
+        maybe_stripe,
+    }
+})();
--- table.js
+++ table.js
@@ -1,3 +1,10 @@
+const {
+    dom_empty_table,
+    dom_tr,
+    dom_td,
+    maybe_stripe,
+} = window.dom_helpers;
+
 build_person_table();
 build_fruits_table();
 build_prime_table();
@@ -90,40 +97,6 @@ function simple_table_widget({
     repopulate();
 
     return { scroll_container, table, repopulate, resize_list };
-}
-
-function dom_empty_table() {
-    const table = document.createElement("table");
-    const thead = document.createElement("thead");
-    table.append(thead);
-
-    const tbody = document.createElement("tbody");
-    table.append(tbody);
-
-    return { table, thead, tbody };
-}
-
-function dom_tr(...child_elems) {
-    const tr = document.createElement("tr");
-    tr.append(...child_elems);
-    return tr;
-}
-
-function dom_td({ id, elem }) {
-    const td = document.createElement("td");
-    td.id = id;
-    td.append(elem);
-    return td;
-}
-
-function maybe_stripe(elem, i, color) {
-    if (i % 2) {
-        setStyles(elem, {
-            background: color,
-        });
-    }
-
-    return elem;
 }
 
 function wire_up_reverse_button({ th, callback }) {
